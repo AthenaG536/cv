4eda00855924bf6be8f3c05fefb56f67f63ca763
Athena <64632419+AthenaG536@users.noreply.github.com>
2021-12-17 13:27:48 +1300

    updated about for relevancy

--- src/Components/About.js
+++ src/Components/About.js
@@ -8,10 +8,8 @@ class About extends Component {
     const name = this.props.data.name;
     const profilepic = "images/" + this.props.data.image;
     const bio = this.props.data.bio;
-    const street = this.props.data.address.street;
     const city = this.props.data.address.city;
-    const state = this.props.data.address.state;
-    const zip = this.props.data.address.zip;
+    const country = this.props.data.address.country;
     const phone = this.props.data.phone;
     const email = this.props.data.email;
     const resumeDownload = this.props.data.resumedownload;
@@ -24,7 +22,7 @@ class About extends Component {
               <img
                 className="profile-pic"
                 src={profilepic}
-                alt="Nordic Giant Profile Pic"
+                alt="Athena's Profile Pic"
               />
             </div>
             <div className="nine columns main-col">
@@ -38,9 +36,7 @@ class About extends Component {
                     <span>{name}</span>
                     <br />
                     <span>
-                      {street}
-                      <br />
-                      {city} {state}, {zip}
+                      {city}, {country}
                     </span>
                     <br />
                     <span>{phone}</span>
