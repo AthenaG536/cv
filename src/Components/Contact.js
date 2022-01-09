8b977ad053f1aac89bb56ed31422797c7040bacf
Athena <64632419+AthenaG536@users.noreply.github.com>
2022-01-10 11:37:31 +1300

    Update address to just show country

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -6,12 +6,12 @@ class Contact extends Component {
     if (!this.props.data) return null;
 
     const name = this.props.data.name;
-    const street = this.props.data.address.street;
     const city = this.props.data.address.city;
-    const state = this.props.data.address.state;
-    const zip = this.props.data.address.zip;
+    const country = this.props.data.address.country;
     const phone = this.props.data.phone;
     const message = this.props.data.contactmessage;
+    const email = this.props.data.email;
+    const mailtoemail = "mailto:" + this.props.data.email;
 
     return (
       <section id="contact">
@@ -106,45 +106,17 @@ class Contact extends Component {
           <Slide right duration={1000}>
             <aside className="four columns footer-widgets">
               <div className="widget widget_contact">
-                <h4>Address and Phone</h4>
+                <h4>Contact Details</h4>
                 <p className="address">
                   {name}
                   <br />
-                  {street} <br />
-                  {city}, {state} {zip}
-                  <br />
                   <span>{phone}</span>
+                  <br />
+                  <a href={mailtoemail}><span>{email}</span></a>
                 </p>
               </div>
 
-              <div className="widget widget_tweets">
-                <h4 className="widget-title">Latest Tweets</h4>
-                <ul id="twitter">
-                  <li>
-                    <span>
-                      This is Photoshop's version of Lorem Ipsum. Proin gravida
-                      nibh vel velit auctor aliquet. Aenean sollicitudin, lorem
-                      quis bibendum auctor, nisi elit consequat ipsum
-                      <a href="./">http://t.co/CGIrdxIlI3</a>
-                    </span>
-                    <b>
-                      <a href="./">2 Days Ago</a>
-                    </b>
-                  </li>
-                  <li>
-                    <span>
-                      Sed ut perspiciatis unde omnis iste natus error sit
-                      voluptatem accusantium doloremque laudantium, totam rem
-                      aperiam, eaque ipsa quae ab illo inventore veritatis et
-                      quasi
-                      <a href="./">http://t.co/CGIrdxIlI3</a>
-                    </span>
-                    <b>
-                      <a href="./">3 Days Ago</a>
-                    </b>
-                  </li>
-                </ul>
-              </div>
+
             </aside>
           </Slide>
         </div>
