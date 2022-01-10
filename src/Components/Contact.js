cb7fbe68e0da3ef506f30e5f13037275ee6d3764
Athena <64632419+AthenaG536@users.noreply.github.com>
2022-01-11 12:00:33 +1300

    Rearranging contact.js & footer.js and hiding contact form temporarily to release working version faster

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -5,6 +5,16 @@ class Contact extends Component {
   render() {
     if (!this.props.data) return null;
 
+    const networks = this.props.data.social.map(function (network) {
+      return (
+          <li key={network.name}>
+            <a href={network.url}>
+              <i className={network.className}></i>
+            </a>
+          </li>
+      );
+    });
+
     const name = this.props.data.name;
     const city = this.props.data.address.city;
     const country = this.props.data.address.country;
@@ -14,113 +24,145 @@ class Contact extends Component {
     const mailtoemail = "mailto:" + this.props.data.email;
 
     return (
-      <section id="contact">
-        <Fade bottom duration={1000}>
-          <div className="row section-head">
-            <div className="two columns header-col">
-              <h1>
-                <span>Get In Touch.</span>
-              </h1>
+        <section id="contact">
+          <Fade bottom duration={1000}>
+            <div className="row section-head">
+              <div className="two columns header-col">
+                <h2 className="primarylight">
+                  <span>Get In Touch.</span>
+                </h2>
+              </div>
+
+              <div className="ten columns">
+                <p className="lead">{message}</p>
+              </div>
             </div>
+          </Fade>
 
-            <div className="ten columns">
-              <p className="lead">{message}</p>
-            </div>
-          </div>
-        </Fade>
+          <div className="row hide">
+            <Slide left duration={1000}>
+              <div className="eight columns">
+                <form action="" method="post" id="contactForm" name="contactForm">
+                  <fieldset>
+                    <div>
+                      <label htmlFor="contactName">
+                        Name <span className="required">*</span>
+                      </label>
+                      <input
+                          type="text"
+                          defaultValue=""
+                          size="35"
+                          id="contactName"
+                          name="contactName"
+                          onChange={this.handleChange}
+                      />
+                    </div>
 
-        <div className="row">
-          <Slide left duration={1000}>
-            <div className="eight columns">
-              <form action="" method="post" id="contactForm" name="contactForm">
-                <fieldset>
-                  <div>
-                    <label htmlFor="contactName">
-                      Name <span className="required">*</span>
-                    </label>
-                    <input
-                      type="text"
-                      defaultValue=""
-                      size="35"
-                      id="contactName"
-                      name="contactName"
-                      onChange={this.handleChange}
-                    />
-                  </div>
+                    <div>
+                      <label htmlFor="contactEmail">
+                        Email <span className="required">*</span>
+                      </label>
+                      <input
+                          type="text"
+                          defaultValue=""
+                          size="35"
+                          id="contactEmail"
+                          name="contactEmail"
+                          onChange={this.handleChange}
+                      />
+                    </div>
 
-                  <div>
-                    <label htmlFor="contactEmail">
-                      Email <span className="required">*</span>
-                    </label>
-                    <input
-                      type="text"
-                      defaultValue=""
-                      size="35"
-                      id="contactEmail"
-                      name="contactEmail"
-                      onChange={this.handleChange}
-                    />
-                  </div>
+                    <div>
+                      <label htmlFor="contactSubject">Subject</label>
+                      <input
+                          type="text"
+                          defaultValue=""
+                          size="35"
+                          id="contactSubject"
+                          name="contactSubject"
+                          onChange={this.handleChange}
+                      />
+                    </div>
 
-                  <div>
-                    <label htmlFor="contactSubject">Subject</label>
-                    <input
-                      type="text"
-                      defaultValue=""
-                      size="35"
-                      id="contactSubject"
-                      name="contactSubject"
-                      onChange={this.handleChange}
-                    />
-                  </div>
+                    <div>
+                      <label htmlFor="contactMessage">
+                        Message <span className="required">*</span>
+                      </label>
+                      <textarea
+                          cols="50"
+                          rows="15"
+                          id="contactMessage"
+                          name="contactMessage"
+                      ></textarea>
+                    </div>
 
-                  <div>
-                    <label htmlFor="contactMessage">
-                      Message <span className="required">*</span>
-                    </label>
-                    <textarea
-                      cols="50"
-                      rows="15"
-                      id="contactMessage"
-                      name="contactMessage"
-                    ></textarea>
-                  </div>
-
-                  <div>
-                    <button className="submit">Submit</button>
-                    <span id="image-loader">
+                    <div>
+                      <button className="submit">Submit</button>
+                      <span id="image-loader">
                       <img alt="" src="images/loader.gif" />
                     </span>
+                    </div>
+                  </fieldset>
+                </form>
+
+                <div id="message-warning"> Error boy</div>
+                <div id="message-success">
+                  <i className="fa fa-check"></i>Your message was sent, thank you!
+                  <br />
+                </div>
+              </div>
+            </Slide>
+
+            <Slide right duration={1000}>
+              <aside className="four columns footer-widgets">
+                <div className="widget widget_contact">
+                  <h4>Contact Details</h4>
+                  <p className="address">
+                    {name}
+                    <br />
+                    <span>{phone}</span>
+                    <br />
+                    <a href={mailtoemail}><span>{email}</span></a>
+                  </p>
+                </div>
+
+
+              </aside>
+            </Slide>
+          </div>
+
+
+          <div className="row">
+            <Slide left duration={1000}>
+
+            </Slide>
+
+            <Slide right duration={1000}>
+              <aside className="twelve columns footer-widgets">
+                <div className="widget widget_contact">
+                  <h2 className="primarylight align-center">Connect with me</h2>
+                  <div className="five columns">
+                    <h2>
+                      <ul className="social-links">{networks}</ul>
+                    </h2>
                   </div>
-                </fieldset>
-              </form>
 
-              <div id="message-warning"> Error boy</div>
-              <div id="message-success">
-                <i className="fa fa-check"></i>Your message was sent, thank you!
-                <br />
-              </div>
-            </div>
-          </Slide>
-
-          <Slide right duration={1000}>
-            <aside className="four columns footer-widgets">
-              <div className="widget widget_contact">
-                <h4>Contact Details</h4>
-                <p className="address">
-                  {name}
-                  <br />
-                  <span>{phone}</span>
-                  <br />
-                  <a href={mailtoemail}><span>{email}</span></a>
-                </p>
-              </div>
+                  <div className="seven columns">
+                    <p className="address">
+                      {name}
+                      <br />
+                      <span>{phone}</span>
+                      <br />
+                      <a href={mailtoemail}><span>{email}</span></a>
+                    </p>
+                  </div>
+                </div>
 
 
-            </aside>
-          </Slide>
-        </div>
-      </section>
+              </aside>
+            </Slide>
+          </div>
+        </section>
     );
   }
 }
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,24 +3,14 @@ import Fade from "react-reveal";
 
 class Footer extends Component {
   render() {
-    if (!this.props.data) return null;
-
-    const networks = this.props.data.social.map(function (network) {
-      return (
-        <li key={network.name}>
-          <a href={network.url}>
-            <i className={network.className}></i>
-          </a>
-        </li>
-      );
-    });
 
     return (
       <footer>
         <div className="row">
           <Fade bottom>
             <div className="twelve columns">
-              <ul className="social-links">{networks}</ul>
+              {/* Repositioned networks to contact.js until contact form is setup */}
+              {/*<ul className="social-links">{networks}</ul>*/}
 
               <ul className="copyright">
                 <li>&copy; Copyright 2021 Athena Gardner</li>
