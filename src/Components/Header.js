726209fa1948f7f7eccfa4a1c80d89e1e693988b
Athena <64632419+AthenaG536@users.noreply.github.com>
2022-01-11 19:10:00 +1300

    Updated classnames and changed background to match theme

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -13,7 +13,7 @@ class Header extends Component {
 
     return (
       <header id="home">
-        <ParticlesBg type="circle" bg={true} />
+        <ParticlesBg color="#160052" type="circle" bg={true} />
 
         <nav id="nav-wrap">
           <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
@@ -67,10 +67,10 @@ class Header extends Component {
             <hr />
             <Fade bottom duration={2000}>
               <ul className="social">
-                <a href={project} className="button btn project-btn">
+                <a href={project} className="button btn header_project_btn">
                   <i className="fa fa-book"></i>Project
                 </a>
-                <a href={github} className="button btn github-btn">
+                <a href={github} className="button btn header_github_btn">
                   <i className="fa fa-github"></i>Github
                 </a>
               </ul>
