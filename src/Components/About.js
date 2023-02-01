66570030de78399b5e24b522b2571bab06184466
Athena Gardner <athena536@hotmail.com>
2023-02-01 20:35:48 +1300

    Rename cv filename in download link

--- src/Components/About.js
+++ src/Components/About.js
@@ -48,7 +48,7 @@ class About extends Component {
                 </div>
                 <div className="columns download">
                   <p>
-                    <a href={resumeDownload} className="button" download="Athena-Gardner_CV_2022.pdf">
+                    <a href={resumeDownload} className="button" download="Athena-Gardner_CV_2023.pdf">
                       <i className="fa fa-download"></i>Download CV
                     </a>
                   </p>
